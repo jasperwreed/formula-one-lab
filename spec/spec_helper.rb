89ec55fcb9f8e73727975e7510eb014af1d848a3
jasperwreed <jmixon.reed@gmail.com>
2021-02-05 16:22:34 -0800

    change sponser.rb to sponsor.rb + configure .rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require_relative '../lib/driver.rb'
+require_relative '../lib/sponsor.rb'
+require_relative ',,/lib/team.rb'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
